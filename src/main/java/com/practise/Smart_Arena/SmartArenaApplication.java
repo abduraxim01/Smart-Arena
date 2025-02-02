f39e79ddeb530cee7b10609b48474f5001cc0d05
Abduraxim <150508362+abduraxim01@users.noreply.github.com>
2025-02-02 17:45:20 +0500

    Add files via upload

---
+++ src/main/java/com/practise/Smart_Arena/SmartArenaApplication.java
@@ -0,0 +1,13 @@
+package com.practise.Smart_Arena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartArenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/Smart_Arena/SmartArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.Smart_Arena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
